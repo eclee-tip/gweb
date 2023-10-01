5b950940442d52798c3be0a8f9573929b26eaab0
eclee-tip <qecblee@tip.edu.ph>
2023-10-01 13:14:39 +0800

    Updated new Rock Paper Scissor Game

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const mazeButton = document.getElementById("mazeButton");
 const snakeButton = document.getElementById("snakeButton");
+const rpsButton = document.getElementById("rpsButton");
 const gameContainer = document.getElementById("gameContainer");
+
+rpsButton.addEventListener("click", () => {
+    // Load rock paper scissor game here
+    gameContainer.innerHTML = '<iframe src="/RPSGame/rps_game.html" width="800" height="600"></iframe>';
+})
 
 mazeButton.addEventListener("click", () => {
     // Load the maze game here
-    gameContainer.innerHTML = '<iframe src="/Maze/maze_game.html" width="800" height="600"></iframe>';
+    gameContainer.innerHTML = '<iframe src="/MazeGame/maze_game.html" width="800" height="600"></iframe>';
 });
 
 snakeButton.addEventListener("click", () => {
